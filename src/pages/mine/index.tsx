0f1d61fe5cd25d1ad9e1b1b41c1422344745d7ee
caisiwei <1374632238@qq.com>
2023-01-06 18:16:18 +0800

    feat：我的

--- src/pages/mine/index.tsx
+++ src/pages/mine/index.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { View, Image, Text } from "@tarojs/components";
 import BasePage from "@/components/base-page";
-import EXAMPLE from "@/images/coupons.png";
 import { useAppSelector } from "@/redux/hooks";
+import Taro from "@tarojs/taro";
 import RIGHT_ARROW from "@/images/right_arrow.png";
 import Operation from "./operation";
 import styles from "./index.module.less";
@@ -10,7 +10,10 @@ import WatchMovie from "./watch-movie";
 
 const MinePage = () => {
   const { avatarUrl, name } = useAppSelector((state) => state.user);
-  console.log(avatarUrl);
+
+  const goPersonal = () => {
+    Taro.navigateTo({ url: "/pages/personal-page/index" });
+  };
   return (
     <BasePage className={styles.page}>
       <View className={styles.userInfo}>
@@ -18,7 +21,7 @@ const MinePage = () => {
           <Image src={avatarUrl} mode="aspectFill" className={styles.avatar} />
           <Text className={styles.name}>{name}</Text>
         </View>
-        <View className={styles.userHome}>
+        <View className={styles.userHome} onClick={goPersonal}>
           个人主页
           <Image src={RIGHT_ARROW} mode="aspectFit" className={styles.arrow} />
         </View>
